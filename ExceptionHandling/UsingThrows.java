717da688c7a70701c64c0880287a9c65c5f4178c
Sumit Maurya <mauryasumit01@gmail.com>
2025-05-29 19:14:53 +0530

    throws tutorial

---
+++ ExceptionHandling/UsingThrows.java
@@ -0,0 +1,35 @@
+package ExceptionHandling;
+import java.io.File;
+import java.io.*;
+public class UsingThrows{
+    static class ReadAndWrite {
+        public void readFile() throws FileNotFoundException {
+            FileInputStream fis = new FileInputStream("abcd.txt");
+            // additional reading logic here
+        }
+
+        public void writeFile() throws FileNotFoundException {
+            FileOutputStream fos = new FileOutputStream("abcd.txt");
+            // additional writing logic here
+        }
+    }
+public static void main(String[] args){
+        ReadAndWrite rw = new ReadAndWrite();
+        try {
+            rw.readFile();
+
+        }catch(FileNotFoundException e){
+            System.out.println("File Not Found to read ");
+            e.printStackTrace();
+        }
+
+        //write
+    try{
+        rw.writeFile();
+    }
+    catch(FileNotFoundException e){
+        System.out.println("File not found to write ..");
+        System.out.println(e);
+    }
+}
+}
