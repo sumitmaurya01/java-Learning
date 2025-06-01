101265a2d70bac2afec7035d031f08ec061edb23
Sumit Maurya <mauryasumit01@gmail.com>
2025-06-01 09:59:50 +0530

    add replace, replaceFirst , replaceAll methods

--- Strings/Methods.java
+++ Strings/Methods.java
@@ -16,5 +16,45 @@ public class Methods {
         System.out.println("before trim length = "+str.length()+ " is Empty = "+str.isEmpty());
         str=str.trim();
         System.out.println(" after trim lenght = "+str.length() +" is empty = "+str.isEmpty() );
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n");
+        System.out.println("Equals ");
+        s1 = "RaN"; s2="Raz";
+        System.out.println("is s1 == s2 "+s1.equals(s2));
+        System.out.println("is s1 euqals s2 ignore case  ="+s1.equalsIgnoreCase(s2));
+        System.out.println("compare s1 and s2 "+s1.compareTo(s2));
+        System.out.println("compare s1 and s2 ignore case  "+s1.compareToIgnoreCase(s2));
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        //System.out.println("sumit"+10-5);
+        System.out.println("sumit"+10*5);
+        s1="sumit";s2=" Maurya"; String s3 = "Sum";
+
+        System.out.println(s1.concat(s2));
+        System.out.println(String.join("_ADIT_",s1,s2,s3));
+// subSequence and substring
+        s1="this is demo string";
+        System.out.println(s1);
+        System.out.println("Subsequence-> "+s1.subSequence(3,10));
+        System.out.println("subString with beginning idx ->"+s1.substring(3));
+        System.out.println("subString with beginning and end idx ->"+s1.substring(3,10));
+        CharSequence ch = "charSequence";
+        String str1="String";
+        System.out.println(ch);
+        System.out.println(str1);
+
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        // replace  , replace first , replaceAll
+        // replace -> replace all the words or character with the given(new word/char)
+        // replaceFirst -> replaces the first occurrence of the given word
+        // replaceAll -> replaces all the occurrence of the given word and it takes regex ((.) , (.*))
+        str = "this is demp";
+        System.out.println("STRING -> "+str);
+        System.out.println("replace-> "+str.replace("is","WAS"));
+        System.out.println("replace first->"+str.replaceFirst("is","WAS"));
+        System.out.println("replace all-> "+str.replaceAll("is","WAS"));//replace all without regex
+        System.out.println("replace all -> "+str.replaceAll("is(.)","WAS"));//replace all with regex
+        System.out.println("Replace all -> "+str.replaceAll("is(.*)","WAS"));//replace all with regex
+
+
     }
 }
